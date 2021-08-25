142982a1246cc4b33964b2acd8faab4aa48b9aa6
Andrew <andreykonovalov33rus@gmail.com>
2021-08-25 22:51:55 +0300

    Lesson_9

--- src/main/java/com/geekbrains/webapp/springapp/controllers/CategoryController.java
+++ src/main/java/com/geekbrains/webapp/springapp/controllers/CategoryController.java
@@ -1,21 +1,26 @@
 package com.geekbrains.webapp.springapp.controllers;
 
 import com.geekbrains.webapp.springapp.dtos.CategoryDto;
+import com.geekbrains.webapp.springapp.exceptions.ResourseNotFoundException;
 import com.geekbrains.webapp.springapp.models.Category;
+import com.geekbrains.webapp.springapp.models.Product;
 import com.geekbrains.webapp.springapp.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @GetMapping("/categories/{id}")
+    @GetMapping("/{id}")
     public CategoryDto findById(@PathVariable Long id){
-        return new CategoryDto(categoryService.findByIdWithProducts(id).get());
+        Category category = categoryService.findById(id).orElseThrow(() -> new ResourseNotFoundException("Category id = " + id + " not found"));
+        return new CategoryDto(category);
     }
 
 }
